4854e17c14ca98f2a58b1c4d738b79211b631708
Chai147258 <146951675+Chai147258@users.noreply.github.com>
2026-02-05 15:09:22 +0700

    Update menu.js

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,32 +1,31 @@
-import { db } from "./app.js";
-import {
-  collection,
-  getDocs,
-  query,
-  where
-} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+const menus = [
+  {
+    id:1,
+    name:"ข้าวกะเพรา",
+    price:50,
+    img:"https://images.unsplash.com/photo-1604908554164-7a5c59f2d82d"
+  },
+  {
+    id:2,
+    name:"ชาเย็น",
+    price:30,
+    img:"https://images.unsplash.com/photo-1542444459-db63c79be30c"
+  }
+];
 
-async function loadMenu(type, targetId){
-  const q = query(collection(db, "menus"), where("type", "==", type));
-  const snapshot = await getDocs(q);
-  const box = document.getElementById(targetId);
+const menuBox = document.getElementById("menu");
 
-  box.innerHTML = "";
-
-  snapshot.forEach(doc=>{
-    const m = doc.data();
-    box.innerHTML += `
-      <div class="item">
-        <img src="${m.image}">
-        <div class="info">
-          <h4>${m.name_th}</h4>
-          <p>฿${m.price}</p>
-          <a class="order-btn" href="https://lin.ee/P7ziHYL">สั่งเมนูนี้</a>
-        </div>
+menus.forEach(item=>{
+  menuBox.innerHTML += `
+    <div class="item">
+      <img src="${item.img}">
+      <div class="info">
+        <h4>${item.name}</h4>
+        <p>${item.price} บาท</p>
+        <button class="order-btn" onclick='addToCart(${JSON.stringify(item)})'>
+          ใส่ตะกร้า
+        </button>
       </div>
-    `;
-  });
-}
-
-loadMenu("food","foodMenu");
-loadMenu("drink","drinkMenu");
+    </div>
+  `;
+});
