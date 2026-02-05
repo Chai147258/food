63d241064f26f2b42cd2a354052238951535c10e
Chai147258 <146951675+Chai147258@users.noreply.github.com>
2026-02-05 15:17:58 +0700

    Update menu.js

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,31 +1,60 @@
-const menus = [
-  {
-    id:1,
-    name:"ข้าวกะเพรา",
-    price:50,
-    img:"https://images.unsplash.com/photo-1604908554164-7a5c59f2d82d"
-  },
-  {
-    id:2,
-    name:"ชาเย็น",
-    price:30,
-    img:"https://images.unsplash.com/photo-1542444459-db63c79be30c"
-  }
-];
+import { db } from "./app.js";
+import { collection, getDocs, query, where } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-const menuBox = document.getElementById("menu");
+async function loadMenu(){
+  const foodQ = query(collection(db, "menus"), where("type","==","food"));
+  const drinkQ = query(collection(db, "menus"), where("type","==","drink"));
 
-menus.forEach(item=>{
-  menuBox.innerHTML += `
-    <div class="item">
-      <img src="${item.img}">
-      <div class="info">
-        <h4>${item.name}</h4>
-        <p>${item.price} บาท</p>
-        <button class="order-btn" onclick='addToCart(${JSON.stringify(item)})'>
-          ใส่ตะกร้า
-        </button>
+  const foodSnap = await getDocs(foodQ);
+  const drinkSnap = await getDocs(drinkQ);
+
+  const foodBox = document.getElementById("foodMenu");
+  const drinkBox = document.getElementById("drinkMenu");
+
+  foodBox.innerHTML = "";
+  drinkBox.innerHTML = "";
+
+  foodSnap.forEach(doc=>{
+    const m = doc.data();
+    foodBox.innerHTML += `
+      <div class="item">
+        <img src="${m.image}">
+        <div class="info">
+          <h4>${m.name_th}</h4>
+          <p>${m.price} บาท</p>
+          <button class="order-btn"
+            onclick='addToCart({
+              id:"${doc.id}",
+              name:"${m.name_th}",
+              price:${m.price}
+            })'>
+            ใส่ตะกร้า
+          </button>
+        </div>
       </div>
-    </div>
-  `;
-});
+    `;
+  });
+
+  drinkSnap.forEach(doc=>{
+    const m = doc.data();
+    drinkBox.innerHTML += `
+      <div class="item">
+        <img src="${m.image}">
+        <div class="info">
+          <h4>${m.name_th}</h4>
+          <p>${m.price} บาท</p>
+          <button class="order-btn"
+            onclick='addToCart({
+              id:"${doc.id}",
+              name:"${m.name_th}",
+              price:${m.price}
+            })'>
+            ใส่ตะกร้า
+          </button>
+        </div>
+      </div>
+    `;
+  });
+}
+
+loadMenu();
